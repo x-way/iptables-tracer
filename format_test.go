6f33c8e8738105f1680e30b8548041f73c5b09a2
Andreas Jaggi <andreas.jaggi@waterwave.ch>
2018-11-11 15:37:49 +0100

    add unit tests for TCP format

--- format_test.go
+++ format_test.go
@@ -27,3 +27,31 @@ func TestFormatPacketICMPv4(t *testing.T) {
 		}
 	}
 }
+
+func TestFormatPacketTCP(t *testing.T) {
+	tables := []struct {
+		tcp      *layers.TCP
+		src      string
+		dst      string
+		length   int
+		expected string
+	}{
+		{&layers.TCP{}, "test-src", "test-dst", 1234, "test-src.0 > test-dst.0: Flags [none], seq 0, win 0, length 1234"},
+		{&layers.TCP{Seq: 999, Window: 95}, "test-src", "test-dst", 1234, "test-src.0 > test-dst.0: Flags [none], seq 999, win 95, length 1234"},
+		{&layers.TCP{DataOffset: 4}, "test-src", "test-dst", 1234, "test-src.0 > test-dst.0: Flags [none], seq 0, win 0, length 1218"},
+		{&layers.TCP{SYN: true}, "test-src", "test-dst", 1234, "test-src.0 > test-dst.0: Flags [S], seq 0, win 0, length 1234"},
+		{&layers.TCP{SYN: true, ACK: true}, "test-src", "test-dst", 1234, "test-src.0 > test-dst.0: Flags [S.], seq 0, win 0, length 1234"},
+		{&layers.TCP{ACK: true}, "test-src", "test-dst", 1234, "test-src.0 > test-dst.0: Flags [.], seq 0, win 0, length 1234"},
+		{&layers.TCP{PSH: true, ACK: true}, "test-src", "test-dst", 1234, "test-src.0 > test-dst.0: Flags [P.], seq 0, win 0, length 1234"},
+		{&layers.TCP{FIN: true}, "test-src", "test-dst", 1234, "test-src.0 > test-dst.0: Flags [F], seq 0, win 0, length 1234"},
+		{&layers.TCP{FIN: true, ACK: true}, "test-src", "test-dst", 1234, "test-src.0 > test-dst.0: Flags [F.], seq 0, win 0, length 1234"},
+		{&layers.TCP{FIN: true, SYN: true, RST: true, PSH: true, ACK: true, URG: true, ECE: true, CWR: true, NS: true}, "test-src", "test-dst", 1234, "test-src.0 > test-dst.0: Flags [FSRP.UEWN], seq 0, win 0, length 1234"},
+	}
+
+	for _, table := range tables {
+		got := formatPacketTCP(table.tcp, table.src, table.dst, table.length)
+		if got != table.expected {
+			t.Errorf("formatPacketTCP was incorrect, got: '%s', expected: '%s'.", got, table.expected)
+		}
+	}
+}
