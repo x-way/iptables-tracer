35bdfcf093da84309b8da128a0de3e191c353e46
Andreas Jaggi <andreas.jaggi@waterwave.ch>
2018-11-24 06:17:00 +0100

    add pcap format tests

---
+++ format_pcap_test.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"os/exec"
+	"path/filepath"
+	"testing"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+	"github.com/google/gopacket/pcap"
+)
+
+func getTcpdumpOutput(filename string) string {
+	out, err := exec.Command("tcpdump", "-S", "-t", "-n", "-r", filename).Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(out)
+}
+
+func getFormatPacketOutput(filename string) string {
+	handle, err := pcap.OpenOffline(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer handle.Close()
+	out := ""
+	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+	for packet := range packetSource.Packets() {
+		if ip4 := packet.Layer(layers.LayerTypeIPv4); ip4 != nil {
+			out = out + formatPacket(append(ip4.LayerContents(), ip4.LayerPayload()...), false) + "\n"
+		} else if ip6 := packet.Layer(layers.LayerTypeIPv6); ip6 != nil {
+			out = out + formatPacket(append(ip6.LayerContents(), ip6.LayerPayload()...), true) + "\n"
+		} else {
+			log.Fatal("Non-IP packet found in " + filename)
+		}
+	}
+	return out
+}
+
+func TestFormatPacketPCAP(t *testing.T) {
+	files, err := ioutil.ReadDir("./tests")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, f := range files {
+		if filepath.Ext(f.Name()) == "pcap" {
+			expected := getTcpdumpOutput("./tests/" + f.Name())
+			got := getFormatPacketOutput("./tests/" + f.Name())
+			if got != expected {
+				t.Errorf("pcap test failed for %s, got '%s', expected '%s'", f.Name(), got, expected)
+			}
+		}
+	}
+}
